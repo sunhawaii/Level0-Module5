6e5bf02ec14356ac3857eb888216c005ce1bd190
sunhawaii <101529828+sunhawaii@users.noreply.github.com>
2022-09-10 15:25:49 -0700

    Update _a_prime_or_not.py

--- _02_algorithms/_a_prime_or_not.py
+++ _02_algorithms/_a_prime_or_not.py
@@ -6,10 +6,32 @@ from tkinter import messagebox, simpledialog, Tk
 
 
 if __name__ == '__main__':
+    window=Tk()
+    window.withdraw()
     # TODO)
     #  1. Ask the user for a number
+    num1=simpledialog.askstring(title=None, prompt= "What number do you want to test for it being prime or not?")
     #  2. Use a for loop, if statement, and modulo to find if the number
     #     is prime.
+    num_2=2
+    while num_2<99999999999999999999999999:
+
+        if num1/num_2 == 1 and num1:
+            var_1 = True
+        if num1 % num_2 == 0:
+            var_2 = True
+
+        if var_1 == True and var_2 == False:
+            messagebox.showinfo(title=None, prompt= "Your number ' + str(num1) + ' is prime.")
+            exit()
+
+        elif var_1 == True and var_2 == True:
+                messagebox.showinfo(title=None, prompt="Your number ' +  str(num1) + ' isn't prime")
+                exit()
+
+        else:
+            num_2=num_2+1
+
     #  3. If the number is divisible by any number other than 1 or itself,
     #     the number is not prime.
     pass
